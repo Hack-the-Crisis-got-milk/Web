16bd9746c9cdbd8c2a9648405bf5c8d2c0e1c82e
MartynasCepas <martynas.cepas@ktu.edu>
2020-03-21 12:58:40 +0200

    Added wishlist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
 
+import React, { Component } from 'react';
+import Checkbox from './Checkbox';
 import MapWithAMarker from './MapWithAMarker';
+import Grid from '@material-ui/core/Grid';
+
+const items = [
+  'Milk',
+  'Bread',
+  'Water',
+];
+
 
  export class App extends React.Component {
     constructor(props) {
@@ -32,18 +41,73 @@ import MapWithAMarker from './MapWithAMarker';
       } 
     }
   
+    componentWillMount = () => {
+      this.selectedCheckboxes = new Set();
+    }
+  
   
     componentDidMount() {
-      this.showCurrentLocation()
+      this.showCurrentLocation();
     }
+
+    toggleCheckbox = label => {
+      if (this.selectedCheckboxes.has(label)) {
+        this.selectedCheckboxes.delete(label);
+      } else {
+        this.selectedCheckboxes.add(label);
+      }
+    }
+  
+    handleFormSubmit = formSubmitEvent => {
+      formSubmitEvent.preventDefault();
+  
+      for (const checkbox of this.selectedCheckboxes) {
+        console.log(checkbox, 'is selected.');
+      }
+    }
+  
+    createCheckbox = label => (
+      <Checkbox
+        label={label}
+        handleCheckboxChange={this.toggleCheckbox}
+        key={label}
+      />
+    )
+  
+    createCheckboxes = () => (
+      items.map(this.createCheckbox)
+    )
   
     render() {
       return (
-        <div style = {{width: "70vw", height: "100vh"}}>
+        <div>
+          <Grid container spacing={12} style = {{width: "100vw", height: "50vw"}}>
+          <Grid item xs={8}>
           <MapWithAMarker
             isMarkerShown={this.state.isMarkerShown}
             currentLocation={this.state.currentLatLng} />
+           </Grid>
+           <Grid item xs={4} >
+              <div className="container" position ="absolute">
+              <div className="row">
+              <div className="col-sm-12">
+                <div>
+                  <h1>Wishlist</h1>
+                </div>
+
+                <form onSubmit={this.handleFormSubmit}>
+                  {this.createCheckboxes()}
+
+                <button className="btn btn-default" type="submit">Save</button>
+
+                </form>
+
+          </div>
         </div>
+      </div>
+      </Grid>
+        </Grid>
+      </div>
       );
     }
   };
